3bef9a0159e58aeba288167fe21974e1b151fca8
chuaqian <chuaqian11@gmail.com>
2024-04-26 14:46:10 +0800

    Finished up

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -1,6 +1,8 @@
 package sg.edu.np.mad.madpractical2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.jar.Attributes;
+
 public class MainActivity extends AppCompatActivity {
+    private static  final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        User user = new User("John Doe", "MAD Developer",1,false);
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button button = findViewById(R.id.button);
+
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        button.setText("Follow");
+
+
+
+
+
+
+
     }
 }
