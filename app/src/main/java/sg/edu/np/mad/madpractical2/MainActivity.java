cbeaa36c65cbeb1503d01d203f6bfd2516cc1a07
chuaqian <chuaqian11@gmail.com>
2024-04-27 13:24:54 +0800

    Finished up

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -8,12 +8,13 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
-    private static  final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
